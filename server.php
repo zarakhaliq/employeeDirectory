9df0ffb3e909720241fa11b074544dfc30f2d074
zarakhaliq <66020363+zarakhaliq@users.noreply.github.com>
2020-10-27 00:06:37 +0000

    Add files via upload

---
+++ server.php
@@ -0,0 +1,6 @@
+<?php
+//development connection (local):
+    $conn= mysqli_connect('eu-cdbr-west-03.cleardb.net', 'bc02a48d2e1ed4', 'f13e04da', 'heroku_3b46370ed5ceeda') or die(mysqli_error($con));
+
+//Remote database connection:
+  ?>
